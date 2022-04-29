0a9abb44a9ab93c700230c53ebdce81986c7eb67
riskypratamablank <104625489+riskypratamablank@users.noreply.github.com>
2022-04-29 19:45:08 +0800

    Update high_pass_filtering.py

--- high_pass_filtering.py
+++ high_pass_filtering.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('jpg',0)
+img = cv2.imread('download.jpg',0)
 f = np.fft.fft2(img)
 fshift = np.fft.fftshift(f)
 
