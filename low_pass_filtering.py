e2bab99e6ac2f3f796470b3e88884f952a5403b1
riskypratamablank <104625489+riskypratamablank@users.noreply.github.com>
2022-04-29 19:45:47 +0800

    Update low_pass_filtering.py

--- low_pass_filtering.py
+++ low_pass_filtering.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('jpg',0)
+img = cv2.imread('download.jpg',0)
 dft = cv2.dft(np.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT)
 dft_shift = np.fft.fftshift(dft)
 rows, cols = img.shape
